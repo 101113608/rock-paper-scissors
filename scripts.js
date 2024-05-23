d8cfeaf7ab123907372fff43fca8cfc8c0034e07
101113608 <48575130+101113608@users.noreply.github.com>
2024-05-23 13:39:48 +1000

    Implement getHumanChoice function
    This function will take an integer input from the user and return
    a string value of 'rock', 'paper', or 'scissors' based on the user's
    input.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 10) % 3 + 1;
 
-    if(choice == 1) {
+    if (choice == 1) {
         return "rock";
     }
     else if (choice == 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = "";
+    while (!(choice === 1 || choice === 2 || choice === 3)) {
+
+        choice = Number(prompt(`
+        Input one of the following numbers to select your move:
+        1 - Rock
+        2 - Paper
+        3 - Scissors
+        `));
+    }
+
+    if (choice === 1) {
+        return "rock";
+    } else if (choice === 2) {
         return "paper";
     }
     else {
