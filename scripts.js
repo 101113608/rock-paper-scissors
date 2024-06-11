dd9191446e9681e2cb9ca5970762d72841d6cff2
101113608 <48575130+101113608@users.noreply.github.com>
2024-06-11 15:47:00 +1000

    Remove getHumanChoice function
    As the game will be played with buttons, the getHumanChoice function
    was removed. Since the function was reliant on taking in an integer
    input from the alert function to determine the player's choice, it was
    no longer needed.

--- scripts.js
+++ scripts.js
@@ -52,28 +52,6 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = "";
-    while (!(choice === 1 || choice === 2 || choice === 3)) {
-
-        choice = Number(prompt(`
-        Input one of the following numbers to select your move:
-        1 - Rock
-        2 - Paper
-        3 - Scissors
-        `));
-    }
-
-    if (choice === 1) {
-        return "rock";
-    } else if (choice === 2) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-
 function playRound(humanChoice, computerChoice) {
     let humanWon = false;
     switch (humanChoice) {
